cc6119efbe1a7bb6617b5fb070ed5f8fa075c2d2
paulVer777 <pawel.gitara@onet.eu>
2018-04-30 21:01:11 +0200

    state added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            value: null
+        }
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -48,7 +56,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -62,6 +70,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
